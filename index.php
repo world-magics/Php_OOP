1a1545662c83e4c10da3083c146db3361c0862c7
Yarashev Sherzod Alisher o'g'li <12.1-11_YSA_1@ytit.local>
2023-03-13 16:33:23 +0500

    do interface implement

--- index.php
+++ index.php
@@ -1,119 +1,31 @@
 <?php
+//Interfaces
+//interfaceda hamma metodla public bulshi kk
+//interface da funksiyala public buladi doim
+   interface Animal{
+        public function makeSound();
+        public function eat();
 
-   abstract class Car{
-        public $model;
-        protected $year;
-        private $km;
+   }
+   //interface ishlatish boshqa clasda implements suzi orqali buladi
+   //shu interfaceni ishlatgan har bitta clasda shu interface ichidagi funksiya bulshi shart dgani
+   //interfaceda faqat funsiyala yozsh mmk atribut yoshb bumydi
 
-        public function __construct($model, $year,$km){
-            $this->model=$model;
-            $this->year=$year;
-            $this->km=$km;
+   class Dog implements Animal{
+        public function makeSound(){
+            return 'Dog Sound';
         }
-
-        public function drive(){
-            return 'driving';
+        public function eat(){
+            return 'Dog eat meat';
         }
-        // public function getFuel(){
-        //     return 'fueling';
-        // }
-
-        public function setModel($model){
-            $this->model=$model;
-        }
-        public function setYear($year){
-            $this->year=$year;
-        }
-        public function getYear(){
-            return $this->year;
-        }
-        public function getKm(){
-            return $this->km;
-        }
-
-        protected function reverse(){
-            return 'reverse drive';
-
-        }
-        private function sell(){
-            return 'selling';
-        }
-        //metod abstrak busa tanasi bumidi
-                // public yoiki protected busa voris olayotgan metod ham shun tipda bulshi kk dgani
-        abstract public function stop();
+   }
+   class Cat implements Animal{
+    public function makeSound(){
+        return 'Cat sound';
     }
-//abstrac klas ichidagi abtrsak metod bulsa bola clasdayam aynan shu metod bulishi kk
-//ichida btta abstrac metodi buldmi clasniyam abstrac qlb quysh kk
-    class ElectricCar extends Car{
-        public function stop(){
-            return 'stop';
-        }
-
+    public function eat(){
+        return 'Cat drink milk';
     }
-    $lucid=new ElectricCar('lucid','2022',10);
-    
-    var_dump($lucid);
-
-
-//     $cherry=new Car('chevrolet',2019,180000);
-//     echo $cherry->getKm('125000');
-
-//     echo('</br>');
-
-
-//     echo $cherry->getYear('2020');
-
-
-
-// class ElectricCar extends Car{
-//     public $km;
-//     public function getYear(){
-//         return $this->year;
-//     }
-//     public function getKm(){
-//         return $this->km;
-//     }
-//     public function getReverse(){
-//         return $this->reverse();
-//     }
-// }
-
-// $tesla=new ElectricCar('tesla','2020','2644684');
-
-// echo '</br>'.$tesla->getReverse();
-
-// echo '</br>'.$tesla->getYear();
-
-
-    // class ElectricCar extends Car{
-    //     public $battery;
-
-    //     public function charge(){
-    //         return 'charging';
-    //     }
-    //     //overrise method drive in ElectricCar after Car class in drive method
-    //     public function drive(){ 
-    //         return 'electric driving';
-    //     }
-
-    // }
-    // class RaceCar extends Car{
-
-    // }
-    // class PublicCar extends Car{
-
-    // }
-    // $tesla=new ElectricCar('Tesla','2025-11-25');
-    // $tesla->battery='5000';
-    // var_dump($tesla->drive());
-
-    // $bmw=new Car('bmw','2023-23-12');
-
-    // $audi=new Car('Audi','2025-23-23');
-    // var_dump($bmw);
-    // echo('</br>');
-    // echo('</br>');
-    // var_dump($audi);
-    
+   }
     
 ?>
