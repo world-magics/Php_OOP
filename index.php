23e7e1dfc4b11c97444eccf84447625e65b30701
Yarashev Sherzod Alisher o'g'li <12.1-11_YSA_1@ytit.local>
2023-03-14 15:33:08 +0500

    traits about how to work

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
-//Interfaces
-//interfaceda hamma metodla public bulshi kk
-//interface da funksiyala public buladi doim
+//Trait
+//bitta funksiyani bir nechta clasda ishlatish manoni bildiradi
+
+
+trait AnimalTrait{
+    public function eat(){
+        return 'Cat eat meat and fat';
+    }
+    public function sleep(){
+        return 'sleeping';
+    }
+}
+trait Walking{
+    public function walking(){
+            return 'walking';
+    }
+}
+
    interface Animal{
         public function makeSound();
         public function eat();
@@ -15,17 +30,25 @@
         public function makeSound(){
             return 'Dog Sound';
         }
-        public function eat(){
-            return 'Dog eat meat';
-        }
+        use AnimalTrait;
+       
    }
    class Cat implements Animal{
     public function makeSound(){
         return 'Cat sound';
     }
-    public function eat(){
-        return 'Cat drink milk';
-    }
+    use AnimalTrait;
+   
    }
+   class Kitten extends Cat {
+        use AnimalTrait,Walking;
+   }
+   $dog =new Dog;
+   $cat=new kitten;
+
+   var_dump($cat->walking());
+   var_dump($dog->makeSound());
+   var_dump($dog->eat());
+   var_dump($dog->sleep());
     
 ?>
