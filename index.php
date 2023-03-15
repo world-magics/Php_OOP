fb3a33eeea669d983938870e9be33864376de7a9
Yarashev Sherzod Alisher o'g'li <12.1-11_YSA_1@ytit.local>
2023-03-15 14:52:33 +0500

    add static method

--- index.php
+++ index.php
@@ -1,54 +1,30 @@
 <?php
-//Trait
-//bitta funksiyani bir nechta clasda ishlatish manoni bildiradi
+
+//Static method and Property
+    class Dog{
+        public $name;
+        public static $weight='25 kg';
 
 
-trait AnimalTrait{
-    public function eat(){
-        return 'Cat eat meat and fat';
-    }
-    public function sleep(){
-        return 'sleeping';
-    }
-}
-trait Walking{
-    public function walking(){
-            return 'walking';
-    }
-}
-
-   interface Animal{
-        public function makeSound();
-        public function eat();
-
-   }
-   //interface ishlatish boshqa clasda implements suzi orqali buladi
-   //shu interfaceni ishlatgan har bitta clasda shu interface ichidagi funksiya bulshi shart dgani
-   //interfaceda faqat funsiyala yozsh mmk atribut yoshb bumydi
-
-   class Dog implements Animal{
-        public function makeSound(){
-            return 'Dog Sound';
+        public function bark(){
+            $this->run();
+            $this->eat();
+            return 'dog sounds';
         }
-        use AnimalTrait;
-       
-   }
-   class Cat implements Animal{
-    public function makeSound(){
-        return 'Cat sound';
+        public static function eat(){
+            echo 'eating';
+        }
+        public function run(){
+            echo 'running';
+        }
     }
-    use AnimalTrait;
-   
-   }
-   class Kitten extends Cat {
-        use AnimalTrait,Walking;
-   }
-   $dog =new Dog;
-   $cat=new kitten;
 
-   var_dump($cat->walking());
-   var_dump($dog->makeSound());
-   var_dump($dog->eat());
-   var_dump($dog->sleep());
+    // $bingo=new Dog();
+    // $bingo->name="Bingo";
+
+    // echo $bingo->bark();
+
+    echo Dog::eat();
+    echo Dog::$weight;
     
 ?>
